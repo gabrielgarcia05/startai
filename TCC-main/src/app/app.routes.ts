55a489a16348917256e214e747260cbb95893006
Gabriel Garcia <100931438+gabrielgarcia05@users.noreply.github.com>
2024-08-12 12:03:04 -0300

    Delete TCC-main directory

--- TCC-main/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from '../pages/home/home.component';
-import { MemoriaComponent } from '../pages/jogos/memoria/memoria.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'jogo-memoria', component: MemoriaComponent },
-
-];
